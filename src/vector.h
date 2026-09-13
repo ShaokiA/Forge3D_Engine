ba36b812f2273afa085f1282a661464c7b0d49ca
ahnafabik5 <ahnafabik5@gmail.com>
2026-09-13 20:18:56 +0600

    Add comments to vector header

--- src/vector.h
+++ src/vector.h
@@ -1,36 +1,35 @@
-#ifndef VECTOR_H
-#define VECTOR_H
+#ifndef VECTOR_H                    // Header age include na holei(if) shudhu abar include kore define korbo
+#define VECTOR_H                    // Header include korse
 
-typedef struct {
-    float x;
-    float y;
-} vec2;
+typedef struct {                    // 2D vector er structure
+    float x;                        // X coordinate
+    float y;                        // Y coordinate
+} vec2;                             // struct er nam vec2
 
-typedef struct {
-    float x;
-    float y;
-    float z;
-} vec3;
+typedef struct {                    // 3D vector er structure
+    float x;                        // X coordinate
+    float y;                        // Y coordinate
+    float z;                        // Z coordinate
+} vec3;                             // struct er nam vec3
 
-typedef struct {
-    float x;
-    float y;
-    float z;
-    float w;
-} vec4;
+typedef struct {                    // 4D vector er structure
+    float x;                        // X coordinate
+    float y;                        // Y coordinate
+    float z;                        // Z coordinate
+    float w;                        // Homogeneous coordinate: w=0 hole position ar w=1 hole coordinate vector
+                                        transformation er jonno use kora hoy
+} vec4;                             // struct er nam vec4
 
-typedef struct {
-    float x, y, z;
-} vertex2d;
+typedef struct {                    // Vertex er structure
+    float x, y, z;                  // X, Y and Z values
+} vertex2d;                         // struct er nam vertex2d, eta 3D jodio name dewa hoise 2D karon 2D theke 3D te newar jonno
 
-vec3 vec3_add(vec3 a, vec3 b);
-vec3 vec3_sub(vec3 a, vec3 b);
-vec3 vec3_scale(vec3 v, float s);
+vec3 vec3_add(vec3 a, vec3 b);      // Duita 3D vector jog kore
+vec3 vec3_sub(vec3 a, vec3 b);      // Duita 3D vector biyog kore
+vec3 vec3_scale(vec3 v, float s);   // Vector ke ekta number diye gun kore
+float vec3_dot(vec3 a, vec3 b);     // Dot product calculate kore
+vec3 vec3_cross(vec3 a, vec3 b);    // Cross product calculate kore
+float vec3_length(vec3 v);          // Vector er length calculate kore
+vec3 vec3_normalize(vec3 v);        // Vector ke unit vector banay
 
-float vec3_dot(vec3 a, vec3 b);
-vec3 vec3_cross(vec3 a, vec3 b);
-
-float vec3_length(vec3 v);
-vec3 vec3_normalize(vec3 v);
-
-#endif
+#endif                              // Header guard sesh
